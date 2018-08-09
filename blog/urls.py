0a908ad159b34bc60647036327f8456dcdc48c19
Wee Liang <weeliangng@hotmail.com>
2018-08-09 21:54:08 +0800

    added login function

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
 	path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
 	path('post/new', views.post_new, name = 'post_new'),
 	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-	url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
-	url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
-	url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+	path('drafts/', views.post_draft_list, name='post_draft_list'),
+	path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+	path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+
 ]
