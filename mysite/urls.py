4639ab9c703d54e83803ce71dccb46103bbc66d5
Wee Liang <weeliangng@hotmail.com>
2018-08-09 22:07:44 +0800

    change import view as

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('accounts/login/', views.login, name='login'),
-    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
+    path('accounts/login/', auth_views.login, name='login'),
+    path('accounts/logout/', auth_views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
