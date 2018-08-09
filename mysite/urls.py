1183db604b4cc8603f21eb54c10d8e50b55d424f
Wee Liang <weeliangng@hotmail.com>
2018-08-09 22:58:03 +0800

    LoginView LogoutView

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    url(r'^accounts/login/$', auth_views.login, name='login'),
-    url(r'^accounts/logout/$', auth_views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
